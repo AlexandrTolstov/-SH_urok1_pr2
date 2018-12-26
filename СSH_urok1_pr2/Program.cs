ad7b04837accd499ddf8ed6bc127a7e244b2e739
AlexandrTolstov <alextav_85@mail.ru>
2018-12-26 20:39:53 +0600

    1

--- СSH_urok1_pr2/Program.cs
+++ СSH_urok1_pr2/Program.cs
@@ -10,6 +10,20 @@ namespace СSH_urok1_pr2
     {
         static void Main(string[] args)
         {
+            Decimal devidend = decimal.One;
+            Console.WriteLine(devidend);
+            decimal devisor = 3;
+            devidend = devidend / devisor;
+            Console.WriteLine(devidend);
+            Console.WriteLine(devidend * devisor);
+            Console.WriteLine("Round");
+            Console.WriteLine(Math.Round(devidend * devisor));
+            double doubleDevidend = 1;
+            Console.WriteLine(doubleDevidend);
+            System.Double doubleDevisor = 3;
+            doubleDevidend = doubleDevidend / doubleDevisor;
+            Console.WriteLine(doubleDevidend);
+            Console.WriteLine(doubleDevidend * doubleDevisor);
         }
     }
 }
